9e93181bf0f4b26b8e7577358473476c68742394
kocherovaarina <arinakocerova@gmail.com>
2026-04-25 17:40:35 +0300

    Task done by Kocherova Arina from 25 IST 4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,66 @@
 // Copyright 2021 NNTU-CS
+
+#ifndef INCLUDE_TPQUEUE_H_
+#define INCLUDE_TPQUEUE_H_
+
+#include <stdexcept>
+
+template<typename T>
+class TPQueue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* head;
+
+public:
+    TPQueue() : head(nullptr) {}
+
+    ~TPQueue() {
+        while (head) {
+            pop();
+        }
+    }
+
+    void push(const T& value) {
+        Node* newNode = new Node(value);
+        if (!head || head->data.prior < value.prior) {
+            newNode->next = head;
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next && current->next->data.prior >= value.prior) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+
+    T pop() {
+        if (!head) {
+            throw std::runtime_error("Queue is empty");
+        }
+        Node* temp = head;
+        T returnValue = head->data;
+        head = head->next;
+        delete temp;
+        return returnValue;
+    }
+
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+};
+
+struct SYM {
+    char ch;
+    int prior;
+};
+
+#endif
+

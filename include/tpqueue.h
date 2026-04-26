1079a29ca0efa2426b5433cf32a3f3bcd0cc79e0
kocherovaarina <arinakocerova@gmail.com>
2026-04-26 12:45:18 +0300

    Task done by Kocherova Arina from 25 IST 4-2

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,83 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+struct SYM {
+    char ch;
+    int prior;
 };
 
-struct SYM {
-  char ch;
-  int prior;
+template<typename T>
+class TPQueue {
+ private:
+    struct Node {
+        T data;
+        Node* next;
+        explicit Node(const T& value) : data(value), next(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
+    int size;
+
+ public:
+    TPQueue() : head(nullptr), tail(nullptr), size(0) {}
+
+    ~TPQueue() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    void push(const T& item) {
+        Node* newNode = new Node(item);
+
+        if (isEmpty() || item.prior > head->data.prior) {
+            newNode->next = head;
+            head = newNode;
+            if (tail == nullptr) {
+                tail = head;
+            }
+        } else {
+            Node* current = head;
+            while (current->next != nullptr &&
+                   current->next->data.prior >= item.prior) {
+                current = current->next;
+            }
+            newNode->next = current->next;
+            current->next = newNode;
+
+            if (newNode->next == nullptr) {
+                tail = newNode;
+            }
+        }
+        size++;
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            throw "Queue is empty";
+        }
+
+        Node* temp = head;
+        T result = head->data;
+        head = head->next;
+
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+
+        delete temp;
+        size--;
+        return result;
+    }
+
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+
+    int getSize() const {
+        return size;
+    }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
